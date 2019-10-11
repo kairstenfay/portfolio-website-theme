e13e5e6dc5ec856b9f57976907201cff3ae13c5d
Kairsten Fay <kairsten.fay@gmail.com>
2019-10-10 23:48:50 -0700

    Create nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import profilePicture from './me.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi, I'm Kairsten</h1>
+        <nav>
+          <li>home</li>
+          <li>about</li>
+          <li>cv</li>
+          <li>blog</li>
+          <li>email</li>
+          <li>github</li>
+          <li>linked in</li>
+        </nav>
+        <img id="profile-pic" src={profilePicture} alt="My face" />
       </header>
+
     </div>
   );
 }
